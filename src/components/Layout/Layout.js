8354def149145e54a42c777ef3a26fbecfc2f572
emmanueljose <emjose@gmail.com>
2021-11-22 21:38:10 -0500

    update dependencies
    git push

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,87 +1,77 @@
 import Head from "next/head";
-import Link from 'next/link'
-import styles from './Layout.module.css';
+import Link from "next/link";
+import styles from "./Layout.module.css";
 import { useEffect, useState } from "react";
-import FlareRoundedIcon from '@material-ui/icons/FlareRounded';
+import FlareRoundedIcon from "@material-ui/icons/FlareRounded";
 
 const Layout = ({ children, title = "Global Metrics" }) => {
+	const [theme, setTheme] = useState("light");
 
-  const [theme, setTheme] = useState("light");
+	useEffect(() => {
+		document.documentElement.setAttribute("data-theme", localStorage.getItem("theme"));
 
-  useEffect(() => {
-    document.documentElement.setAttribute(
-      "data-theme",
-      localStorage.getItem("theme")
-    );
+		setTheme(localStorage.getItem("theme"));
+	}, []);
 
-    setTheme(localStorage.getItem("theme"));
-  }, []);
+	const switchTheme = () => {
+		if (theme === "light") {
+			saveTheme("dark");
+		} else {
+			saveTheme("light");
+		}
+	};
 
-  const switchTheme = () => {
-    if (theme === "light") {
-      saveTheme("dark");
-    } else {
-      saveTheme("light");
-    }
-  };
+	const saveTheme = (theme) => {
+		setTheme(theme);
+		localStorage.setItem("theme", theme);
+		document.documentElement.setAttribute("data-theme", theme);
+	};
 
-  const saveTheme = (theme) => {
-    setTheme(theme);
-    localStorage.setItem("theme", theme);
-    document.documentElement.setAttribute("data-theme", theme);
-  };
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>{title}</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-  return (
+			<header className={styles.header}>
+				<div>
+					<div className={styles.buttoncontainer}>
+						<button
+							className={styles.themeSwitcher}
+							onClick={switchTheme}
+							tabIndex="0"
+							style={{ cursor: "pointer" }}
+						>
+							<FlareRoundedIcon style={{ fontSize: 30 }} alt="switch to light or dark mode" />
+						</button>
+					</div>
 
-    <div className={styles.container}>
-      <Head>
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
-      <header className={styles.header}>
+					<Link href="/">
+						<a>
+							<img className={styles.logo} src="/gm-logo.png" alt="Global Metrics" />
+						</a>
+					</Link>
+				</div>
+			</header>
 
-        <div>
+			<main className={styles.main}>{children}</main>
 
-          <div className={styles.buttoncontainer}>
-            <button className={styles.themeSwitcher} onClick={switchTheme} tabIndex="0" style={{ cursor: "pointer" }}>
-              <FlareRoundedIcon style={{ fontSize: 30 }} alt="switch to light or dark mode"/>
-              
-            </button>
-          </div>
-
-          <Link href="/">
-            <a>
-              <img className={styles.logo} src="/gm-logo.png" alt="Global Metrics"/>
-            </a>
-          </Link>
-          
-        </div>
-
-      </header>
-
-      <main className={styles.main}>
-        {children}
-      </main>
-
-      <footer className={styles.footer}>
-        <Link href="https://github.com/emjose" target="_blank" rel="noopener noreferrer">
-          <a>
-            Made by Emmanuel Jose
-          </a>
-        </Link>
-        &nbsp;•&nbsp;
-        <Link href="https://github.com/emjose/global-metrics-app/#header" target="_blank" rel="noopener noreferrer">
-          <a>
-            View Github
-          </a>
-        </Link>
-      </footer>
-
-    </div>
-    
-  );
+			<footer className={styles.footer}>
+				<Link href="https://github.com/emjose" target="_blank" rel="noopener noreferrer">
+					<a>Made by Emmanuel Jose</a>
+				</Link>
+				&nbsp;•&nbsp;
+				<Link
+					href="https://github.com/emjose/global-metrics-app/#header"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<a>View Github</a>
+				</Link>
+			</footer>
+		</div>
+	);
 };
 
 export default Layout;
-
