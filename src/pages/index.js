3f612d6826a8dde0c45e18ffd692ebbb742c9be0
emmanueljose <emjose@gmail.com>
2022-09-08 21:01:33 -0400

    deployment test 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,7 @@ export default function Home({ countries }) {
 
 // export const getStaticProps = async () => {
 export const getStaticProps = async () => {
-	const res = await fetch("https://restcountries.com/v3/all");
+	const res = await fetch("https://restcountries.com/v2/all");
 	const countries = await res.json();
 
 	return {
