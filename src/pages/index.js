526fe13d1ad0e896d131030651e5e73d7dd3db84
emmanueljose <emjose@gmail.com>
2022-09-08 21:03:25 -0400

    update api

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,7 @@ export default function Home({ countries }) {
 
 // export const getStaticProps = async () => {
 export const getStaticProps = async () => {
-	const res = await fetch("https://restcountries.com/v2/all");
+	const res = await fetch("https://restcountries.com/v3.1/all");
 	const countries = await res.json();
 
 	return {
